534c9f845441901b2cc16594f1147b7a849e6fcc
Shirish <shirishaddaganti@gmail.com>
2023-11-17 12:35:07 -0500

    streamlit app skeleton

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Assignment- 4")
+
+st.write("This Streamlit app has two main functionalities:")
+st.subheader("a. Given a text, it retrieves the closest image that fits the text description.")
+st.subheader("b. Given an image, it finds 3 other images that are similar to the uploaded image.")
+st.write("You can choose the desired option from the sidebar.")
